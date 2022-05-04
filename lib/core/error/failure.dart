dd65dc2ad5770084d7e908779db6acbe7bfdd2a2
farukmeral25 <farukmeral25@gmail.com>
2022-05-04 19:09:16 +0300

    Add failures

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,3 +1,36 @@
 abstract class Failure {}
 
+//! General Failures
 class NullPointerFailure implements Failure {}
+
+class ListEmptyFailure implements Failure {}
+
+class UserNotFoundFailure implements Failure {}
+
+//! Base Response Status Failures
+class BadRequestFailure implements Failure {}
+
+class UnauthorizedFailure implements Failure {}
+
+class ForbiddenFailure implements Failure {}
+
+class NotFoundFailure implements Failure {}
+
+class InternalFailure implements Failure {}
+
+class GatewayTimeOutFailure implements Failure {}
+
+extension FailureExtension on Failure {
+  String errorMessage() {
+    switch (runtimeType) {
+      case NullPointerFailure:
+        return "Null Pointer Failure";
+      case ListEmptyFailure:
+        return "List Empty Failure";
+      case UserNotFoundFailure:
+        return "No user found with the entered information.";
+      default:
+        return "Failure";
+    }
+  }
+}
