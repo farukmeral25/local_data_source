0f8090358cc2482de977fcf3b33e0b71c78b8a74
farukmeral25 <farukmeral25@gmail.com>
2022-05-04 18:53:55 +0300

    Add scaffold messanger key.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:local_data_source/core/keys/global_key.dart';
+import 'package:local_data_source/core/keys/scaffold_messanger_key.dart';
 import 'package:local_data_source/core/service/setup_service.dart';
 import 'package:local_data_source/core/utils/route_manager/domain/router.dart'
     as router;
@@ -25,6 +26,7 @@ class LocalDataApp extends StatelessWidget {
     return MaterialApp(
       title: 'Local Data Source',
       navigatorKey: GlobalContextKey.instance.globalKey,
+      scaffoldMessengerKey: GlobalScaffoldMessangerKey.instance.globalKey,
       onGenerateRoute: router.generateRoute,
       theme: ThemeData(
         primarySwatch: Colors.blue,
